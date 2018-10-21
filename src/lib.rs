341d0b13d21baaf23fbe8485016481ef918d035a
Bjørnar Luteberget <luteberget@gmail.com>
2018-10-21 17:19:56 +0200

    doc-example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
 //! SAT solver using [MiniSat](http://minisat.se).
 //! Solves a boolean satisfiability problem given in conjunctive normal form.
+//!
+//! ```rust
+//! extern crate minisat;
+//! fn main() {
+//!     let mut sat = minisat::Sat::new();
+//!     let a = sat.new_lit();
+//!     let b = sat.new_lit();
+//!     sat.add_clause(&[a, !b]);
+//!     sat.add_clause(&[b]);
+//!     match sat.solve() {
+//!         Ok(m) => {
+//!             assert_eq!(m.get(&a), Some(true));
+//!             assert_eq!(m.get(&b), Some(true));
+//!         },
+//!         Err(()) => panic!("UNSAT"),
+//!     }
+//! }
+//! ```
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
